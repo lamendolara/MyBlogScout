c49691339226dbcd18d31dceae57d30946fc5dee
lamendolara <lamendolara@gmail.com>
2024-08-25 17:31:58 +0200

    nvo Ip prod.py

--- blog/blog/settings/prod.py
+++ blog/blog/settings/prod.py
@@ -5,7 +5,7 @@ import os
 DEBUG = False
 
 # Add your allowed hosts here
-ALLOWED_HOSTS = ['161.35.208.71']  # ['your_domain.com', 'your_ip_address']Cambia esto por tu dominio o IP del servidor
+ALLOWED_HOSTS = ['46.101.136.86']  # ['your_domain.com', 'your_ip_address']Cambia esto por tu dominio o IP del servidor
 
 # Database
 DATABASES = {
